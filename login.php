079c12419a05c61fc535bafe5968354d5fa31648
ziyicitu <ziyicitu@gmail.com>
2019-10-22 16:01:40 +0100

    wow mysql is ok

--- login.php
+++ login.php
@@ -1,3 +1,14 @@
 <?php
-echo "hello world!";
+$json_string = file_get_contents("config.json");
+$data = json_decode($json_string,true);
+$dbhost = $data[host];
+$dbuser = $data[user];
+$dbpassword = $data[password];
+$conn = mysqli_connect($dbhost, $dbuser, $dbpassword);
+if($conn != null)
+{
+	echo "yes";
+}
+else
+echo "no";
 ?>
